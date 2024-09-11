c810d4053a0cc706f1478ef0688d19788f250943
Simon Boylan <siboy73@gmail.com>
2024-09-11 19:01:06 +0100

    Add images and style front page

---
+++ js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.fixed-action-btn').floatingActionButton();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'left',
+    hoverEnabled: false
+  });
+});
